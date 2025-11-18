9e2c9fb0dc94c1890a66ac70b7333702a5c6429f
Elisa Raeli <79461498+Elisa-Ra@users.noreply.github.com>
2025-11-18 11:05:45 +0100

    adds react-bootstrap

--- es2-d2/src/App.jsx
+++ es2-d2/src/App.jsx
@@ -1,9 +1,16 @@
 import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container, Row } from "react-bootstrap"
+// import Container from "react-bootstrap/Container"
 
 function App() {
   return (
     <>
-      <h1 className="text-danger">ESERCIZIO 2</h1>
+      <Container>
+        <Row>
+          <h1 className="text-danger">ESERCIZIO 2</h1>
+        </Row>
+      </Container>
     </>
   )
 }
